5f344f02cb85ebf8ca3b54170b8fea9d572a5d94
etuhoha <etuhoha@gmail.com>
2026-05-04 07:31:40 +0300

    register user cpmmand; sqlc: add user queries

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/etuhoha/gator/internal/config"
+	"github.com/etuhoha/gator/internal/database"
+	"github.com/google/uuid"
+
+	_ "github.com/lib/pq"
 )
 
 type state struct {
 	config *config.Config
+	db     *database.Queries
 }
 
 type command struct {
@@ -35,12 +43,20 @@ func (c *commands) register(name string, f func(*state, command) error) {
 func main() {
 	cmds := commands{make(map[string]func(*state, command) error)}
 	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
 
 	conf, err := config.Read()
 	if err != nil {
 		fmt.Printf("read err: %v\n", err)
 	}
-	state := state{&conf}
+
+	db, err := sql.Open("postgres", conf.DbUrl)
+	if err != nil {
+		fmt.Printf("can't open DB connection: %v\n", err)
+		os.Exit(1)
+	}
+
+	state := state{&conf, database.New(db)}
 
 	args := os.Args
 	if len(args) < 2 {
@@ -58,15 +74,47 @@ func main() {
 
 func handlerLogin(s *state, cmd command) error {
 	if len(cmd.args) < 1 {
-		return fmt.Errorf("missing name for login")
+		return fmt.Errorf("missing username for login")
 	}
 
-	user := cmd.args[0]
-	err := s.config.SetUser(user)
+	name := cmd.args[0]
+	_, err := s.db.GetUser(context.Background(), name)
+	if err != nil {
+		return fmt.Errorf("user '%v' not found: [%v]", name, err)
+	}
+
+	err = s.config.SetUser(name)
 	if err != nil {
 		return err
 	}
 
 	fmt.Printf("user set to '%v'\n", s.config.User)
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) < 1 {
+		return fmt.Errorf("missing username for register")
+	}
+
+	name := cmd.args[0]
+
+	params := database.CreateUserParams{}
+	params.Name = name
+	params.ID = uuid.New()
+	params.CreatedAt = time.Now()
+	params.UpdatedAt = params.CreatedAt
+
+	user, err := s.db.CreateUser(context.Background(), params)
+	if err != nil {
+		return err
+	}
+
+	err = s.config.SetUser(name)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("user created: %v\n", user)
 	return nil
 }
